b81cfc34ca3e1aa39d3ae9654c3d672ac4dbbebd
Mr-Sena <jsenna985@gmail.com>
2021-10-11 22:06:29 -0300

    Classes utilitárias
    as classes java.util.Collections e java.util.Arrays (repare o s no final) são utilitárias que possuem métodos auxiliares para trabalhar com coleções e arrays.

--- TrilhaAlura/JavaPuro/Collections/Workspace/src/br/com/Chuckhran/Vetor.java
+++ TrilhaAlura/JavaPuro/Collections/Workspace/src/br/com/Chuckhran/Vetor.java
@@ -15,21 +15,29 @@ public class Vetor {
 
 
 
+
+        ///as classes java.util.Collections e java.util.Arrays (repare o s no final) são utilitárias que
+        // possuem métodos auxiliares para trabalhar com coleções e arrays.
+        // Por exemplo, método Collections.sort(...) serve para ordenadar determinada collections
+
+
+
+        //Arrays.asList, permite adicionar uma serie de elementos à instância, porém, a lista se torna imutável.
+
         List listagem = Arrays.asList("wifhifw", "wkfjwifj", 1);
 
+        //listagem.add(1); -> UnsupportedOperationException
 
+        //Alternativa: passar a collection para outras collection criada normalmente.
 
-        ArrayList arrayList = new ArrayList();
+        ArrayList arrayList = new ArrayList(listagem);
 
         arrayList.add("One");
-        arrayList.addAll(listagem);
 
-        System.out.println("\nArrays.asList: " + listagem + " -> Permite definir valores à instanciação " +
-                ",porém, não é permtido adicionar elementos posteriormente.");
-        System.out.println("ArrayList: " + arrayList + " -> Comportamento inverso ao da Collection acima");
-
-
-
+        System.out.println("\n\nlista primordia - imutável: ");
+        System.out.println(listagem);
+        System.out.println("\nLista desdobrada a partir da primeira: ");
+        System.out.println(arrayList);
     }
 
 }
