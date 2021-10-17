2ac4a93981a31f5993babb83dd81b15722aa0322
Mr-Sena <jsenna985@gmail.com>
2021-10-17 15:47:45 -0300

    Writer-Output

---
+++ TrilhaAlura/JavaPuro/StreamBtyes/Workspace/src/br/com/alura/io/MoreSreamData.java
@@ -0,0 +1,44 @@
+package br.com.alura.io;
+
+import java.io.*;
+import java.net.Socket;
+
+public class MoreSreamData {
+
+    public static void main(String[] args) throws IOException {
+
+        //Outras fluxo de transmissão de dados [ENTRADA/SAÍDA]
+
+        InputStream inputStreamConsole = System.in; //new FileInputStream("AutoType");
+
+        Reader leitorFluxoDeEntrada = new InputStreamReader(inputStreamConsole);
+
+        BufferedReader bufferedReader = new BufferedReader(leitorFluxoDeEntrada);
+
+
+
+        //Preparando o fluxo de saída:
+
+        OutputStream outputStreamConsole = System.out; //FileOutputStream("RealTime.txt");
+
+        Writer osw = new OutputStreamWriter(outputStreamConsole);
+
+        BufferedWriter bw = new BufferedWriter(osw);
+
+
+
+        String linha = bufferedReader.readLine();
+
+        while(linha != null && !linha.isEmpty()) {
+            bw.write(linha);
+            bw.newLine();
+            bw.flush();//fluxo retorna devolta a entrada como saída assim que pressionar o enter com o recurso flush
+            linha = bufferedReader.readLine();
+        }
+
+        bufferedReader.close();
+        bw.close();
+
+    }
+
+}
---
+++ TrilhaAlura/JavaPuro/StreamBtyes/Workspace/src/br/com/alura/io/WriterCase.java
@@ -0,0 +1,27 @@
+package br.com.alura.io;
+
+import java.io.*;
+
+public class WriterCase {
+
+    public static void main(String[] args) throws IOException {
+
+        FileOutputStream fos = new FileOutputStream("AutoType");
+
+        OutputStreamWriter osw = new OutputStreamWriter(fos);
+
+        BufferedWriter bw = new BufferedWriter(osw);
+
+        bw.write("Conexão verdadeiramente é algo mais profundo que por sua complexidade não conseguimos " +
+                "exalar toda à sua completude com fidelidade à autenticidade da mensagem.");
+        bw.newLine();
+        bw.write("O que por vezes, no entanto, artistas consegue retratar com bastante clareza, " +
+                "de uma perspectiva bastante poética um vislumbre nessa realidade mais distante... ");
+
+
+
+        bw.close();
+
+    }
+
+}
