618cbb484b02945dbc87d6b0110d56148d2b74e8
Mr-Sena <jsenna985@gmail.com>
2021-10-31 22:31:55 -0300

    Padronização dos caracteres nas tabela UNICODE

---
+++ TrilhaAlura/JavaPuro/StreamBtyes/Workspace/src/br/com/alura/io/CharacteresCode.java
@@ -0,0 +1,34 @@
+package br.com.alura.io;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class CharacteresCode {
+
+    public static void main(String[] args) throws UnsupportedEncodingException {
+
+        String s = "C";
+        System.out.println("Código da letra C na tabela Unicode: " + s.codePointAt(0));
+
+
+
+        Charset charset = Charset.defaultCharset();
+        System.out.println("Encodificador do código: " + charset);
+
+        byte[] characterBytes = s.getBytes();
+        //Param. opcional, e caso de inanição, é inferido o charset default, nesse caso o UTF-8.
+        System.out.printf("Qtde de bytes: %s \n", characterBytes.length);
+
+        //Conversão reversa - do código para a representação:
+        String byteToDecimal = new String(characterBytes);
+        System.out.println("Representação do byte: " + byteToDecimal);
+
+        String s1 = "13º Órgão Oficial";
+        byte[] bytes = s1.getBytes();
+        String s2 = new String(bytes, "UTF-8");
+        System.out.println(s2);
+
+    }
+
+}
