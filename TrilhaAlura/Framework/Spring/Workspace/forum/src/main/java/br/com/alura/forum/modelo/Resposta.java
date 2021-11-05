4a41c314575e7de412ac45e9066661cdaf0d4d8e
Mr-Sena <jsenna985@gmail.com>
2021-11-04 23:26:19 -0300

    Definição das propriedades da aplicação
     - Configuração do Application properties
     - Modelagem das entidades
     - Adoção do banco de dados em memoria (H2)

--- TrilhaAlura/Framework/Spring/Workspace/forum/src/main/java/br/com/alura/forum/modelo/Resposta.java
+++ TrilhaAlura/Framework/Spring/Workspace/forum/src/main/java/br/com/alura/forum/modelo/Resposta.java
@@ -1,14 +1,25 @@
 package br.com.alura.forum.modelo;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
 public class Resposta {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	private String mensagem;
+
+	@ManyToOne
 	private Topico topico;
+
 	private LocalDateTime dataCriacao = LocalDateTime.now();
+
+	@ManyToOne
 	private Usuario autor;
+
 	private Boolean solucao = false;
 
 	@Override
